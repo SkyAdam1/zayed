341764e15b960ea05523286f24468c9b36a6b03f
Deni <29742498+azureswastika@users.noreply.github.com>
2020-11-16 21:53:07 +0300

    Add LoginRequired and fixed application status switcher

--- apps/applications/views.py
+++ apps/applications/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse_lazy
@@ -16,29 +17,31 @@ def output(request):
     return render(request, 'applications/applications_output.html')
 
 
-class ApplicationsOutputView(View):
+class ApplicationsOutputView(LoginRequiredMixin, View):
+    login_url = reverse_lazy('login_url')
+
     def get(self, request):
         application = None
-        if(request.user.is_authenticated):
-            if(request.user.is_superuser or request.user.is_expert):
-                application = Application.objects.all()
-            elif(request.user.is_active):
-                application = Application.objects.filter(user=request.user)
-            return render(request, 'applications/applications_output.html', context={'application': application})
-        return HttpResponseRedirect(reverse_lazy('login_url'))
+        if(request.user.is_superuser or request.user.is_expert):
+            application = Application.objects.all()
+        elif(request.user.is_active):
+            application = Application.objects.filter(user=request.user)
+        return render(request, 'applications/applications_output.html', context={'application': application})
 
 
-class ApplicationAddExpert(UpdateView):
+class ApplicationAddExpert(LoginRequiredMixin, UpdateView):
     model = Application
     fields = ['designated_expert']
     template_name = 'applications/applications_add_expert.html'
+    login_url = reverse_lazy('login_url')
     success_url = reverse_lazy('applications_output_url')
 
 
-class ApplicationsCreateView(CreateView):
+class ApplicationsCreateView(LoginRequiredMixin, CreateView):
     model = Application
     template_name = 'applications/applications_create.html'
     form_class = forms.ApplicationCreateForm
+    login_url = reverse_lazy('login_url')
     success_url = reverse_lazy('applications_output_url')
 
     def form_valid(self, form):
@@ -48,37 +51,37 @@ class ApplicationsCreateView(CreateView):
         return super().form_valid(form)
 
 
-class ApplicationsDetailView(utils.ObjectDetailMixin, View):
+class ApplicationsDetailView(LoginRequiredMixin, utils.ObjectDetailMixin, View):
     model = Application
     template_name = 'applications/applications_detail.html'
     form_class = forms.ApplicationCommentForm
+    login_url = reverse_lazy('login_url')
     success_url = reverse_lazy('applications_detail_url')
 
 
-class ApplicationsReportingView(View):
+class ApplicationsReportingView(LoginRequiredMixin, View):
     def get(self, request):
         application = None
-        if(request.user.is_authenticated):
-            if(request.user.is_superuser or request.user.is_expert):
-                application = Application.objects.all()
-            elif(request.user.is_active):
-                application = Application.objects.filter(user=request.user)
-            return render(request, 'applications/applications_reporting.html', context={'application': application})
-        return HttpResponseRedirect(reverse_lazy('login_url'))
+        if(request.user.is_superuser or request.user.is_expert):
+            application = Application.objects.all()
+        elif(request.user.is_active):
+            application = Application.objects.filter(user=request.user)
+        return render(request, 'applications/applications_reporting.html', context={'application': application})
 
 
 def switch_application_status(request, id):
     app = get_object_or_404(Application, pk=id)
-    if app.status:
-        app.status = False
-        app.save()
-    else:
-        app.status = True
-        app.save()
+    if app.user == request.user:
+        if app.status:
+            app.status = False
+            app.save()
+        else:
+            app.status = True
+            app.save()
     return HttpResponseRedirect(reverse_lazy('applications_output_url'))
 
 
-class ApplicationUpdateView(UpdateView):
+class ApplicationUpdateView(LoginRequiredMixin, UpdateView):
     model = Application
     fields = [
         'project_name', 'project_site', 'data_project_start', 'legal_entity',
@@ -89,4 +92,5 @@ class ApplicationUpdateView(UpdateView):
         'fio_team', 'team_education', 'team_experience', 'position_member', 'team_create', 'ready_relocate',
         'ready_development', 'adress_company', 'inn_company', 'fio', 'email', 'upload']
     template_name = 'applications/application_update_form.html'
+    login_url = reverse_lazy('login_url')
     success_url = reverse_lazy('applications_output_url')
