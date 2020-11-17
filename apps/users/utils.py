591db7557ab8ead0d4601bd5d5533f77fed392a3
Deni <29742498+azureswastika@users.noreply.github.com>
2020-11-17 03:02:14 +0300

    Fixed Mixin error

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,5 +1,4 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 
@@ -7,4 +6,4 @@ from django.utils.decorators import method_decorator
 class UserAuthenticatedMixin:
     @method_decorator(user_passes_test(lambda u: not u.is_authenticated, login_url=reverse_lazy('applications_output_url')))
     def dispatch(self, *args, **kwargs):
-        return redirect('applications_output_url')
+        return super(UserAuthenticatedMixin, self).dispatch(*args, **kwargs)
