37fa2a77670335d1682e5c60942199dda1bc6430
azureswastika <azureswastika@gmail.com>
2020-12-16 07:52:25 +0300

    Update

--- config/asgi.py
+++ config/asgi.py
@@ -2,6 +2,11 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+from .settings.settings import CONFIG
+
+if CONFIG.settings == 'dev':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.settings')
+elif CONFIG.settings == 'heroku':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.heroku')
 
 application = get_asgi_application()
