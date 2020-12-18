0ec4f14d4969760f8d72d0eada85781561ecdc33
SkyAdam1 <69192973+SkyAdam1@users.noreply.github.com>
2020-12-18 14:11:06 +0300

    Update utils.py

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import user_passes_test
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 
 
 class UserAuthenticatedMixin:
-    @method_decorator(user_passes_test(lambda u: not u.is_authenticated, login_url=reverse_lazy('applications_output_url')))
+    @method_decorator(user_passes_test(lambda u: not u.is_authenticated, login_url=reverse('applications_output_url')))
     def dispatch(self, *args, **kwargs):
         return super(UserAuthenticatedMixin, self).dispatch(*args, **kwargs)
